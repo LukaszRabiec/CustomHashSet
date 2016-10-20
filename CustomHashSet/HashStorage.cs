151c9e7cbd0e5620981ab56735a6e7439ff4478e
Łukasz Rabiec <lukaszrabiec@gmail.com>
2016-10-20 12:52:26 +0200

    Add project files.

---
+++ CustomHashSet/HashStorage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomHashSet
+{
+    public class HashStorage<T> where T : IConvertible
+    {
+        private LinkedList<T>[] _storage;
+
+        public HashStorage()
+        {
+            _storage = new LinkedList<T>[16];
+        }
+
+        public void Add(T element)
+        {
+
+        }
+
+        public void Remove(T element)
+        {
+
+        }
+
+        public void Contains(T element)
+        {
+
+        }
+
+        private int CalculateHash(T element)
+        {
+            int elementHashCode = element.GetHashCode();
+            double constKey = (Math.Sqrt(5) - 1) / 2;
+
+            return (int)Math.Round(elementHashCode * constKey % 1 * _storage.Length);
+        }
+    }
+}
