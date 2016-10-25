1c68b2bc4dee128d34c9b7acaf782e9de8892ad5
Łukasz Rabiec <lukaszrabiec@gmail.com>
2016-10-25 15:04:48 +0200

    Remove, IndexOf in HashStorage

--- CustomHashSet/HashStorage.cs
+++ CustomHashSet/HashStorage.cs
@@ -5,14 +5,14 @@ namespace CustomHashSet
 {
     public class HashStorage<T> where T : IConvertible
     {
-        private const int _initSize = 16;
+        private const int _minSize = 16;
 
         private LinkedList<T>[] _storage;
         private int _numberOfElements;
 
         public HashStorage()
         {
-            _storage = new LinkedList<T>[_initSize];
+            _storage = new LinkedList<T>[_minSize];
             _numberOfElements = 0;
         }
 
@@ -34,7 +34,7 @@ namespace CustomHashSet
             _storage[listIndex].AddLast(element);
             _numberOfElements++;
 
-            if (_numberOfElements > _storage.Length)
+            if (ElementsExceededSize())
             {
                 IncreaseStorage();
             }
@@ -42,9 +42,25 @@ namespace CustomHashSet
             return true;
         }
 
-        public void Remove(T element)
+        public bool Remove(T element)
         {
+            int listIndex = CalculateHash(element);
+            bool elementIsInStorage = Contains(element);
 
+            if (!elementIsInStorage)
+            {
+                return false;
+            }
+
+            _storage[listIndex].Remove(element);
+            _numberOfElements--;
+
+            if (StorageLengthIsLargerThenMinSize() && ElementsDecreasedBy40Percent())
+            {
+                DecreaseStorage();
+            }
+
+            return true;
         }
 
         public bool Contains(T element)
@@ -62,6 +78,27 @@ namespace CustomHashSet
             }
 
             return false;
+        }
+
+        public int IndexOf(T element)
+        {
+            int listIndex = CalculateHash(element);
+
+            if (_storage[listIndex] != null)
+            {
+                int index = 0;
+                foreach (var item in _storage[listIndex])
+                {
+                    if (item.Equals(element))
+                    {
+                        return index;
+                    }
+
+                    index++;
+                }
+            }
+
+            return -1;
         }
 
         private int CalculateHash(T element)
@@ -90,6 +127,41 @@ namespace CustomHashSet
                     }
                 }
             }
+        }
+
+        private void DecreaseStorage()
+        {
+            _numberOfElements = 0;
+            int newSize = _storage.GetLength(0) / 2;
+
+            var oldStorage = _storage;
+            _storage = new LinkedList<T>[newSize];
+
+            for (int i = 0; i < newSize; i++)
+            {
+                if (oldStorage[i] != null)
+                {
+                    foreach (var element in oldStorage[i])
+                    {
+                        Add(element);
+                    }
+                }
+            }
+        }
+
+        private bool ElementsExceededSize()
+        {
+            return _numberOfElements > _storage.Length;
+        }
+
+        private bool StorageLengthIsLargerThenMinSize()
+        {
+            return _storage.GetLength(0) > _minSize;
+        }
+
+        private bool ElementsDecreasedBy40Percent()
+        {
+            return _numberOfElements * 2.5 < _storage.GetLength(0);
         }
     }
 }
